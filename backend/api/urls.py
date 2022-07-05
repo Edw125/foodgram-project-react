af5c9b4aa2ce601db530445e372c80c26b1e3787
Edgar <57034089+Edw125@users.noreply.github.com>
2022-07-06 02:13:06 +0300

    v.1 final

---
+++ backend/api/urls.py
@@ -0,0 +1,14 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from .views import IngredientsViewSet, RecipesViewSet, TagsViewSet
+
+router = DefaultRouter()
+router.register('recipes', RecipesViewSet, basename='recipes')
+router.register('ingredients', IngredientsViewSet, basename='ingredients')
+router.register('tags', TagsViewSet, basename='tags')
+
+urlpatterns = [
+    path('', include(router.urls)),
+
+]
---
+++ backend/api/views.py
@@ -0,0 +1,128 @@
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from django_filters.rest_framework import DjangoFilterBackend
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.pdfgen import canvas
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
+
+from foodgram.settings import MEDIA_ROOT
+from recipes.models import (Favorites, Ingredient, IngredientAmount, Recipe,
+                            ShoppingCart, Tag)
+from .filters import IngredientSearchFilter, RecipeFilter
+from .pagination import CustomPageNumberPagination
+from .permissions import IsAuthorOrReadOnly
+from .serializers import (FavoriteSerializer, IngredientSerializer,
+                          RecipeListSerializer, RecipeSerializer,
+                          ShoppingCartSerializer, TagSerializer)
+
+
+class TagsViewSet(ReadOnlyModelViewSet):
+    queryset = Tag.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = TagSerializer
+
+
+class IngredientsViewSet(ReadOnlyModelViewSet):
+    queryset = Ingredient.objects.all()
+    permission_classes = (AllowAny, )
+    serializer_class = IngredientSerializer
+    filter_backends = [IngredientSearchFilter]
+    search_fields = ('^name',)
+
+
+class RecipesViewSet(ModelViewSet):
+    queryset = Recipe.objects.all()
+    permission_classes = [IsAuthorOrReadOnly]
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = RecipeFilter
+    pagination_class = CustomPageNumberPagination
+
+    def get_serializer_class(self):
+        if self.action in ('list', 'retrieve'):
+            return RecipeListSerializer
+        return RecipeSerializer
+
+    @staticmethod
+    def post_method_for_actions(request, pk, serializers):
+        data = {'user': request.user.id, 'recipe': pk}
+        serializer = serializers(data=data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    @staticmethod
+    def delete_method_for_actions(request, pk, model):
+        user = request.user
+        recipe = get_object_or_404(Recipe, id=pk)
+        model_obj = get_object_or_404(model, user=user, recipe=recipe)
+        model_obj.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    @action(detail=True, methods=["POST"],
+            permission_classes=[IsAuthenticated])
+    def favorite(self, request, pk):
+        return self.post_method_for_actions(
+            request=request, pk=pk, serializers=FavoriteSerializer)
+
+    @favorite.mapping.delete
+    def delete_favorite(self, request, pk):
+        return self.delete_method_for_actions(
+            request=request, pk=pk, model=Favorites)
+
+    @action(detail=True, methods=["POST"],
+            permission_classes=[IsAuthenticated])
+    def shopping_cart(self, request, pk):
+        return self.post_method_for_actions(
+            request=request, pk=pk, serializers=ShoppingCartSerializer)
+
+    @shopping_cart.mapping.delete
+    def delete_shopping_cart(self, request, pk):
+        return self.delete_method_for_actions(
+            request=request, pk=pk, model=ShoppingCart)
+
+    @action(detail=False, methods=['get'],
+            permission_classes=[IsAuthenticated])
+    def download_shopping_cart(self, request):
+        item_list = {}
+        ingredients = IngredientAmount.objects.filter(
+            recipe__carts__user=request.user).values_list(
+            'ingredient__name', 'ingredient__measurement_unit',
+            'amount'
+        )
+        for item in ingredients:
+            name = item[0]
+            if name not in item_list:
+                item_list[name] = {
+                    'measurement_unit': item[1],
+                    'amount': item[2]
+                }
+            else:
+                item_list[name]['amount'] += item[2]
+
+        response = HttpResponse(content_type='application/pdf')
+        response['Content-Disposition'] = ('attachment; '
+                                           'filename="shopping_list.pdf"')
+        return create_pdf(response, item_list)
+
+
+def create_pdf(response, item_list):
+    pdfmetrics.registerFont(TTFont(
+        'TimesNewRoman', MEDIA_ROOT + '/data/TimesNewRoman.ttf', 'UTF-8'
+    ))
+    page = canvas.Canvas(response)
+    page.setFont('TimesNewRoman', size=20)
+    page.drawString(250, 780, 'Список покупок')
+    page.setFont('TimesNewRoman', size=12)
+    height = 725
+    for i, (name, data) in enumerate(item_list.items(), 1):
+        page.drawString(70, height, (f'{i}. {name}: {data["amount"]} '
+                                     f'{data["measurement_unit"]}'))
+        height -= 25
+    page.showPage()
+    page.save()
+    return response
