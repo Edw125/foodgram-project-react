2629cd1dc53fac2c8a414a623726fbc5b1becee7
Edgar <57034089+Edw125@users.noreply.github.com>
2022-07-07 01:38:47 +0300

    SSL fix

--- infra/init-letsencrypt.sh
+++ infra/init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(51.250.21.79 foodgram.servehttp.com)
+domains=(foodgram.servehttp.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="kt215n@gmail.com" # Adding a valid address is strongly recommended
